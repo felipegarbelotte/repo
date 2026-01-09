b28a5bce156a620d45127cb6bbcb840b301e3111
felipegarbelotte <54819647+felipegarbelotte@users.noreply.github.com>
2026-01-09 11:09:29 -0300

    Update simples-financas.py

--- simples-financas.py
+++ simples-financas.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 from dateutil import parser
 import altair as alt
 
